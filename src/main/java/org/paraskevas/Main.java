283f4d0abee5326e5d3785b4b5e4d2fab32ac5c6
Paraskevas Paraskevas <paraskevasp@gmail.com>
2024-09-13 13:55:57 +0100

    adding java and kotlin in same main

--- src/main/java/org/paraskevas/Main.java
+++ src/main/java/org/paraskevas/Main.java
@@ -1,8 +1,13 @@
 package org.paraskevas;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.concurrent.ExecutionException;
 
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        System.out.println("Executing Kotlin code...");
         BillionGenKotlinKt.main();
+        System.out.println("Executing Java code...");
+        BillionNumberGeneratorJava.main(args);
+        System.out.println("Java code execution completed.");
     }
 }
